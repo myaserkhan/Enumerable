860264c8b2c9020fd5b395535fb1236602f6f783
Selma <selmandapanda@yahoo.com>
2023-01-05 15:48:09 +0200

    Verify solution

--- class.rb
+++ class.rb
@@ -13,4 +13,8 @@ class MyList
 end
 
 list = MyList.new(1, 2, 3, 4)
-puts(list.all? { |e| e < 5 })
+p(list.all? { |e| e < 5 })
+p(list.all? { |e| e > 5 })
+p(list.any? { |e| e == 2 })
+p(list.any? { |e| e == 5 })
+p(list.filter(&:even?))
