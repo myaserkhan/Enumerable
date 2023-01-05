820b27b32d76423cd64c837dcc705b10615186d6
Selma <selmandapanda@yahoo.com>
2023-01-05 15:47:14 +0200

    Add any and filter method

--- module.rb
+++ module.rb
@@ -10,4 +10,19 @@ module MyEnumerable
     end
     ary.length == list.length
   end
+
+  def any?
+    each do |i|
+      return true if yield(i)
+    end
+    false
+  end
+
+  def filter
+    result = []
+    each do |i|
+      result << i if yield(i)
+    end
+    result
+  end
 end
